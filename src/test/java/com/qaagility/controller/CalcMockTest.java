3ff5ca489e12378103077f36c469ecfbfe75a831
Umasankarreddy123 <38308928+Umasankarreddy123@users.noreply.github.com>
2018-04-12 15:10:06 +0530

    Update CalcMockTest.java

--- src/test/java/com/qaagility/controller/CalcMockTest.java
+++ src/test/java/com/qaagility/controller/CalcMockTest.java
@@ -1,3 +1,30 @@
+
+Skip to content
+This repository
+
+    Pull requests
+    Issues
+    Marketplace
+    Explore
+
+    @Umasankarreddy123
+
+1
+0
+
+    2
+
+ash1shm/OldCounterWebProject
+Code
+Issues 0
+Pull requests 0
+Projects 0
+Wiki
+Insights
+OldCounterWebProject/src/test/java/com/qaagility/controller/CalcMockTest.java
+36770bb 16 days ago
+@ash1shm ash1shm Update CalcMockTest.java
+79 lines (61 sloc) 1.85 KB
 package com.qaagility.controller;
 
 import com.qaagility.controller.Calculator;
@@ -24,3 +51,68 @@ public class CalcMockTest {
 	@Spy
     	private Calculator calcSpy;
 	
+
+	 @Before
+	 public void setupMock() {
+		 MockitoAnnotations.initMocks(this);
+		//calcMock = mock(Calculator.class);
+	    }	
+
+	@Test
+	public void testCalcMock1()  {
+	System.out.println("Stubbing mul(x,y)"); 	
+	when(calcMock.mul(9,9)).thenReturn(81);
+	assertEquals(81,calcMock.mul(9,9));		
+	verify(calcMock, atLeastOnce()).mul(9,9);	
+        System.out.println("Verifying mock mul method is called atleast once");			
+	when(calcMock.add(6,3)).thenCallRealMethod();	
+	assertEquals(9,calcMock.add(6,3));		
+	verify(calcMock, atLeastOnce()).add(6,3);
+        System.out.println("Verifying real add method is called atleast once");	
+	}
+	
+	@Test
+	public void testCalcSpy1()  {
+	System.out.println("Spying mul(x,y)"); 	
+	when(calcSpy.mul(9,9)).thenReturn(81);
+	assertEquals(81,calcSpy.mul(9,9));
+	verify(calcSpy, atLeastOnce()).mul(9,9);
+        System.out.println("Verifying spy mul method is called atleast once");	
+	}
+	
+
+/*	
+	@Test
+	public void testCalcMock1()  {
+		
+        //  create mock
+	ClassToTest test  = new ClassToTest(calcMock);	
+        // MyCalc test = mock(MyCalc.class);
+        // define return value for method getMul()
+        when(test.getMul()).thenReturn(81);
+        // use mock in test....
+        assertEquals(test.getMul(), 81);
+	}
+*/	
+	@Test	
+      public void testCalc() {
+        assertEquals("Result", 9, 9);
+      }
+	
+    }
+
+    © 2018 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    API
+    Training
+    Shop
+    Blog
+    About
+
+Press h to open a hovercard with more details.
